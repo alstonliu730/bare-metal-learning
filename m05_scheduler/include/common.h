42ebfadeb01cdea3a2d1a644f000eec114e290c4
Alston Liu <alstonliu730@gmail.com>
2026-01-17 19:16:10 -0500

    Added memcpy and memset to utils.

--- m05_scheduler/include/common.h
+++ m05_scheduler/include/common.h
@@ -1,21 +1,25 @@
 #ifndef _COMMON_H
 #define _COMMON_H
 
-// stdint typedef
-typedef unsigned char      uint8_t;
-typedef unsigned short     uint16_t;
-typedef unsigned int       uint32_t;
-typedef unsigned long long uint64_t;
+#include <stdint.h>
+#include <stddef.h>
 
-typedef signed char        int8_t;
-typedef signed short       int16_t;
-typedef signed int         int32_t;
-typedef signed long long   int64_t;
-
-typedef unsigned long      size_t;
-typedef unsigned long      uintptr_t;  // Pointer-sized integer
-
-#define NULL ((void*)0)
 #define PERIPHERAL_BASE         0xFE000000
 
+// Bit manipulation techniques
+#define SHIFT(val, n)           ((uint64_t)(val) << (n))
+#define BIT(n)                  (1ULL << (n))
+#define CLR_BIT(n)              (~BIT((n)))
+#define BIT_MASK(msb, lsb)      ((BIT((msb) - (lsb) + 1) - 1) << lsb)
+#define CLR_MASK(msb, lsb)      (~ BIT_MASK((msb), (lsb)))
+
+// sets the value in the given address
+void *memset(void *dest, int c, size_t n);
+// copy the values from the source to the destination
+void *memcpy(void *dest, const void *src, size_t n);
+
+// Suspend tasks for n amount of cycles
+static inline void delay(volatile uint32_t count) {
+    while (count--) asm("nop");
+}
 #endif /* _COMMON_H */
